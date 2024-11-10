6767c2ac9e62b027b80924c04b6467c3af3cb077
Trung Vu <trungquocvu1009@gmail.com>
2024-11-10 21:45:41 +0700

    UPDATED 10/11

--- Assets/Script/Stats/PlayerStats.cs
+++ Assets/Script/Stats/PlayerStats.cs
@@ -6,13 +6,6 @@ using UnityEngine.UI;
 public class PlayerStats : CharacterStats
 {
     public Slider healthbar;
-
-    private void Start()
-    {
-        healthbar.maxValue = maxHealth;
-    }
-
-    private void Update()
     {
         healthbar.value = currentHealth;
         if (currentHealth <= 0)
